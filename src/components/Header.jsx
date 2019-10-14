c229f19e27022db224465cc8d7fb8fc8ce18fac6
Svyat <perynsky@gmail.com>
2019-10-15 02:15:38 +0300

    release v1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+   return(
+      <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6hIhoCOwJSxZkDCwwPot9ql1mwwjenBu7z68zKaRUzFvnZ_Hp8Q'></img>
+      </header>
+   );
+}
+
+export default Header;
