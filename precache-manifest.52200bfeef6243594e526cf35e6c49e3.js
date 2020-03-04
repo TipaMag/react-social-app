848add744d4195715a55e1ccf84983cd05b9aacc
TipaMag <perynsky@gmail.com>
2020-03-04 15:33:31 +0200

    Updates

---
+++ precache-manifest.52200bfeef6243594e526cf35e6c49e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe7d4a535c605e669deb120115267d7",
+    "url": "//tipamag.github.io/react-social-app/index.html"
+  },
+  {
+    "revision": "ccad676e3fe060365edf",
+    "url": "//tipamag.github.io/react-social-app/static/css/main.f4004b74.chunk.css"
+  },
+  {
+    "revision": "f24a77b2228136a58c10",
+    "url": "//tipamag.github.io/react-social-app/static/js/2.d624a13a.chunk.js"
+  },
+  {
+    "revision": "1b98b700a4867e0978732113f35bf606",
+    "url": "//tipamag.github.io/react-social-app/static/js/2.d624a13a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a931508f68250be67dbc",
+    "url": "//tipamag.github.io/react-social-app/static/js/3.151b7c76.chunk.js"
+  },
+  {
+    "revision": "ccad676e3fe060365edf",
+    "url": "//tipamag.github.io/react-social-app/static/js/main.44116b0c.chunk.js"
+  },
+  {
+    "revision": "25881573f92e0322ea91",
+    "url": "//tipamag.github.io/react-social-app/static/js/runtime-main.945e59e6.js"
+  },
+  {
+    "revision": "5682ea5ffeca5010cc8a35fabac12d49",
+    "url": "//tipamag.github.io/react-social-app/static/media/404-not-found.5682ea5f.jpg"
+  },
+  {
+    "revision": "f6076e2ce66330bfb7e76ffb26986dad",
+    "url": "//tipamag.github.io/react-social-app/static/media/Spinner-preloader.f6076e2c.svg"
+  },
+  {
+    "revision": "7857b9ce131b12a65745c8de0915934a",
+    "url": "//tipamag.github.io/react-social-app/static/media/default-avatar-icon.7857b9ce.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//tipamag.github.io/react-social-app/precache-manifest.01d3f4c639fa4575f8eb94902b6b52ac.js"
+  "//tipamag.github.io/react-social-app/precache-manifest.52200bfeef6243594e526cf35e6c49e3.js"
 );
 
 self.addEventListener('message', (event) => {
